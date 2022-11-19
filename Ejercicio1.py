0e67099320d7457176f4bfadc5765fa7dfc54afe
Manoloserranoo <113981319+Manoloserranoo@users.noreply.github.com>
2022-11-19 21:34:10 +0000

    cambios

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -2,9 +2,9 @@ def operacion(a,b):
   try:
     op = a/b #definimos la operación a realizar
   except ZeroDivisionError:#Usamos ZeroDivisionError para que nos salte un mensaje cuando se divida entre cero ya que no se puede.
-    print("No se puede dividir un número entre cero")
+    print("No se puede dividir",a,"entre",b,)
   else:
-    print("El resultado de dividir",{a},"y",{b},"es:",op.format)#Si no se divide entre cero imprimimos el resultado de la operación propuesta
+    print("El resultado de dividir",a,"y",b,"es:",op)#Si no se divide entre cero imprimimos el resultado de la operación propuesta
   return
 #Damos varios valores a "a" y "b" para hacer varias pruebas  
 operacion(7,0)
--- Ejercicio2.py
+++ Ejercicio2.py
@@ -3,10 +3,10 @@ def numero_lista(numero):
   try:
     lista[numero]#
   except IndexError:#Usamos IndexError para saber si la posición que solicitamos existe en la lista.
-    print("El número está en el intervalo de la lista")
+    print("La posición buscada no está en el intervalo de la lista")
   else:
-    print("El número no está en el intervalo de la lista")#Si se pide imprimir una posición de la cual no dispinemos en la lista imprimiremos el mensaje.
+    print("La posición buscada está en el intervalo de la lista")#Si se pide imprimir una posición de la cual no dispinemos en la lista imprimiremos el mensaje.
     return
 #Damos dos valores para buscar
 numero_lista(10)
-numero_lista(40)
+numero_lista(2)
